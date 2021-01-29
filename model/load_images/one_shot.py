f8451adef66ad0e0e340add99ea540470566921e
RoNiTo0 <verinkayankova3@gmail.com>
2021-01-29 14:14:57 +0100

    model one-shot

--- model/load_images/one_shot.py
+++ model/load_images/one_shot.py
@@ -1,5 +1,58 @@
 import numpy.random as rng
 import numpy as np
+from load_images import load_images
+
+Xtrain, train_classes, _ = load_images()
+Xval = Xtrain[:, 0, :, :]
+val_classes = train_classes[0::Xtrain.shape[1], :]
+
+def get_batch(batch_size, s="train"):
+    """
+    Create batch of n pairs, half same class, half different class
+    """
+    if s == 'train':
+        X = Xtrain
+        categories = train_classes
+    else:
+        X = Xval
+        categories = val_classes
+    n_classes, n_examples, w, h, d = X.shape
+
+    # randomly sample several classes to use in the batch
+    categories = rng.choice(n_classes, size=(batch_size,), replace=False)
+
+    # initialize 2 empty arrays for the input image batch
+    pairs = [np.zeros((batch_size, h, w, d)) for i in range(2)]
+
+    # initialize vector for the targets
+    targets = np.zeros((batch_size,))
+
+    # make one half of it '1's, so 2nd half of batch has same class
+    targets[batch_size // 2:] = 1
+    for i in range(batch_size):
+        category = categories[i]
+        idx_1 = rng.randint(0, n_examples)
+        pairs[0][i, :, :, :] = X[category, idx_1].reshape(w, h, d)
+        idx_2 = rng.randint(0, n_examples)
+
+        # pick images of same class for 1st half, different for 2nd
+        if i >= batch_size // 2:
+            category_2 = category
+        else:
+            # add a random number to the category modulo n classes to ensure 2nd image has a different category
+            category_2 = (category + rng.randint(1, n_classes)) % n_classes
+
+        pairs[1][i, :, :, :] = X[category_2, idx_2].reshape(w, h, d)
+
+    return pairs, targets
+
+def generate(batch_size, s="train"):
+    """
+    a generator for batches, so model.fit_generator can be used.
+    """
+    while True:
+        pairs, targets = get_batch(batch_size,s)
+        yield (pairs, targets)
 
 def make_oneshot_task(N, s="val", language=None):
     """Create pairs of test image, support set for testing N way one-shot learning. """
@@ -9,7 +62,7 @@ def make_oneshot_task(N, s="val", language=None):
     else:
         X = Xval
         categories = val_classes
-    n_classes, n_examples, w, h = X.shape
+    n_classes, n_examples, w, h, d = X.shape
 
     indices = rng.randint(0, n_examples, size=(N,))
     if language is not None:  # if language is specified, select characters for that language
@@ -22,10 +75,10 @@ def make_oneshot_task(N, s="val", language=None):
         categories = rng.choice(range(n_classes), size=(N,), replace=False)
     true_category = categories[0]
     ex1, ex2 = rng.choice(n_examples, replace=False, size=(2,))
-    test_image = np.asarray([X[true_category, ex1, :, :]] * N).reshape(N, w, h, 1)
+    test_image = np.asarray([X[true_category, ex1, :, :]] * N).reshape(N, w, h, d)
     support_set = X[categories, indices, :, :]
     support_set[0, :, :] = X[true_category, ex2]
-    support_set = support_set.reshape(N, w, h, 1)
+    support_set = support_set.reshape(N, w, h, d)
     targets = np.zeros((N,))
     targets[0] = 1
     targets, test_image, support_set = rng.shuffle(targets, test_image, support_set)
