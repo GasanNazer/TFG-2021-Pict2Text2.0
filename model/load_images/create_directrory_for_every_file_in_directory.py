2daa4bace8d805611532a90f34ddb987aa881019
Gasan Nazer <gnazer@meaningcloud.com>
2021-02-20 23:46:55 +0100

    iteration-2

---
+++ model/load_images/create_directrory_for_every_file_in_directory.py
@@ -0,0 +1,17 @@
+import os
+
+def make_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def create_directory_for_every_file_of_directory(folder = "pictograms_test"):
+    for file in os.listdir(folder):
+        folder_name = file.split("-")[0]
+        make_directory(os.path.join(folder, folder_name))
+        os.rename(os.path.join(folder, file), os.path.join(folder, os.path.join(folder_name, file)))
+
+        #os.removedirs(file.split("-")[0])
+
+
+create_directory_for_every_file_of_directory()
