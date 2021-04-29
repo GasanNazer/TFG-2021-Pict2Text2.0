525f1b7442a6b6765b64129730a688a51bf1feb2
Gasan Nazer <gnazer@meaningcloud.com>
2021-04-29 02:55:24 +0200

    run modification

--- API/run.sh
+++ API/run.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 echo "Running the script: "
 
-#tmux a -t flask-app
-
-#x="source venv/bin/activate"
-#eval "$x"
-#y=$(eval "$x")
-#echo "$y" 
+tmux a -t flask-app
 
 source venv/bin/activate
-
-tmux a -t flask-app
 
 authbind --deep gunicorn3 -w 1 --threads 3 --bind 0.0.0.0:80 app:app > log.txt 2>&1 &
 
