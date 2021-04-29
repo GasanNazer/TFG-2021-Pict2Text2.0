1094e21446df4a63806ee6507e559ec653292fac
Gasan Nazer <gnazer@meaningcloud.com>
2021-04-29 15:34:23 +0200

    modifying the run script

--- API/run.sh
+++ API/run.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 echo "Running the script: "
 
-tmux a -t flask-app
-
 source venv/bin/activate
 
-authbind --deep gunicorn3 -w 1 --threads 3 --bind 0.0.0.0:80 app:app > log.txt 2>&1 &
-
-echo "In tmux"
-
-tmux detach
+authbind --deep gunicorn3 -w 1 --threads 3 --bind 0.0.0.0:80 app:app
