11f1791e7edc80b3cc83a975e1289fb6b08b5fe7
Gasan Nazer <gnazer@meaningcloud.com>
2021-06-06 20:38:16 +0200

    correcting run.sh

--- API/run.sh
+++ API/run.sh
@@ -3,4 +3,4 @@ echo "Running the script: "
 
 source venv/bin/activate
 
-authbind --deep gunicorn3 -w 1 --threads 3 --bind 0.0.0.0:80 app:app
+authbind --deep gunicorn -w 1 --threads 3 --bind 0.0.0.0:80 app:app
